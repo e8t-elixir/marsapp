37232862b4ff53be2636a64dcb8eb7a7703209ef
Peter Lau <superpeterlau@outlook.com>
2021-05-26 20:53:01 +0800

    update config

--- config/dev.exs
+++ config/dev.exs
@@ -61,7 +61,9 @@ config :mars, MarsWeb.Endpoint,
       ~r"priv/static/.*(js|css|png|jpeg|jpg|gif|svg)$",
       ~r"priv/gettext/.*(po)$",
       ~r"lib/mars_web/(live|views)/.*(ex)$",
-      ~r"lib/mars_web/templates/.*(eex)$"
+      ~r"lib/mars_web/templates/.*(eex)$",
+      ~r"lib/mars_live/.*(ex)$",
+      ~r"lib/mars_live/.*(eex)$"
     ]
   ]
 
