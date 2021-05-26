3dad81daecf8414beb748a906665131c2a767f97
Peter Lau <superpeterlau@outlook.com>
2021-05-26 19:53:12 +0800

    use dev.custom

--- config/dev.custom.exs
+++ config/dev.custom.exs
@@ -5,3 +5,6 @@ config :mars, Mars.Repo,
   username: "dev",
   password: "xubuntudb",
   database: "mars_app_dev"
+
+config :mars, Mars.Endpoint,
+  http: [port: System.get_env("PORT", "4000") |> String.to_integer()]
